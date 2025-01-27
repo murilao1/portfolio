39aa267361d6cbf992753a749e9f120d59ede0bd
Murilo Nascimento <muriloaugusto580@gmail.com>
2025-01-27 15:57:19 -0300

    feat: add title screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,65 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import React from 'react';
+import styles from './page.module.css';
+import { AnimatePresence, motion, Variants } from 'motion/react';
+
+const container: Variants = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.3,
+    },
+  },
+};
+
+const item: Variants = {
+  hidden: { opacity: 0, y: 90 },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.8,
+    },
+  },
+};
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+      <AnimatePresence>
+        <motion.ul
+          variants={container}
+          initial={'hidden'}
+          animate={'show'}
+          className='flex flex-auto flex-col justify-center font-primary text-8xl font-bold'
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          <div className='flex flex-row gap-10'>
+            <li className='h-[110px] overflow-hidden'>
+              <motion.span variants={item} className='inline-block'>
+                Murilo{' '}
+              </motion.span>
+            </li>
+            <li className='inline-block h-[110px] overflow-hidden'>
+              <motion.span variants={item} className='inline-block'>
+                <span className='font-thin'>Augusto</span>
+              </motion.span>
+            </li>
+          </div>
+          <div className='flex flex-row gap-10'>
+            <li className='inline-block h-[110px] overflow-hidden'>
+              <motion.span variants={item} className='inline-block'>
+                <span className='font-thin'>Pereira </span>
+              </motion.span>
+            </li>
+            <li className='inline-block h-[110px] overflow-hidden'>
+              <motion.span variants={item} className='inline-block'>
+                Nascimento
+              </motion.span>
+            </li>
+          </div>
+        </motion.ul>
+      </AnimatePresence>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    fontFamily: {
+      primary: ['var(--font-funnel-display)'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
