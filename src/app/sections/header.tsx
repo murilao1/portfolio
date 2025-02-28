6fb78121051843b5af3c74b4971979b271d79886
Murilo Nascimento <muriloaugusto580@gmail.com>
2025-02-28 16:55:13 -0300

    refactor: cleanup motion variants

--- src/app/sections/header.tsx
+++ src/app/sections/header.tsx
@@ -1,43 +1,15 @@
 'use client';
 import React, { useEffect } from 'react';
-import { motion, stagger, useAnimate, useInView, Variants } from 'motion/react';
-
-const container: Variants = {
-  hidden: { opacity: 0 },
-  show: {
-    opacity: 1,
-    transition: {
-      staggerChildren: 0.15,
-    },
-  },
-};
-
-const item: Variants = {
-  hidden: { opacity: 0, y: 90 },
-  show: {
-    opacity: 1,
-    y: 0,
-    transition: {
-      duration: 0.6,
-      ease: 'circInOut',
-    },
-  },
-  hover: {
-    y: -8,
-    transition: { ease: 'easeIn' },
-  },
-};
+import { motion, stagger, useAnimate, useInView } from 'motion/react';
 
 export default function Header() {
   const [scope, animate] = useAnimate();
   const isInView = useInView(scope);
 
   const run = async () => {
-    // tentando recriar a funcionalidade das variants e stagger com o hook animate()
-    await animate('ul', { opacity: 1, y: 0 });
     await animate(
       'li span',
-      { opacity: 1, y: 0 },
+      { opacity: [0, 1], y: [90, 0] },
       {
         type: 'spring',
         bounce: 0.5,
@@ -45,6 +17,7 @@ export default function Header() {
         delay: stagger(0.075),
       },
     );
+    await animate('ul', { scale: 0.5 }, { duration: 0.3, ease: 'easeInOut' });
   };
 
   useEffect(() => {
@@ -56,32 +29,19 @@ export default function Header() {
   return (
     <section
       ref={scope}
-      className='flex min-h-[100dvh] min-w-[100dvw] items-center justify-center gap-16 p-20'
+      className='relative flex min-h-[100dvh] min-w-[100dvw] items-center justify-start gap-16 p-20'
     >
-      <motion.ul
-        variants={container}
-        initial={'hidden'}
-        // animate={'show'}
-        className='flex flex-auto flex-col justify-center font-primary text-8xl font-bold'
-      >
+      <motion.ul className='flex flex-auto origin-left flex-col justify-center font-primary text-8xl font-bold'>
         <div className='flex flex-row gap-10'>
           <li className='h-[110px] overflow-hidden'>
-            <motion.span
-              variants={item}
-              // whileHover={'hover'}
-              className='inline-block'
-            >
+            <motion.span className='inline-block'>
               <span className='transition-all duration-500 ease-in-out hover:font-black'>
                 Murilo
               </span>
             </motion.span>
           </li>
           <li className='inline-block h-[110px] overflow-hidden'>
-            <motion.span
-              variants={item}
-              // whileHover={'hover'}
-              className='inline-block'
-            >
+            <motion.span className='inline-block'>
               <span className='font-thin transition-all duration-500 ease-in-out hover:font-medium'>
                 Augusto
               </span>
@@ -90,22 +50,14 @@ export default function Header() {
         </div>
         <div className='flex flex-row gap-10'>
           <li className='inline-block h-[110px] overflow-hidden'>
-            <motion.span
-              variants={item}
-              // whileHover={'hover'}
-              className='inline-block'
-            >
+            <motion.span className='inline-block'>
               <span className='font-thin transition-all duration-500 ease-in-out hover:font-normal'>
                 Pereira{' '}
               </span>
             </motion.span>
           </li>
           <li className='inline-block h-[110px] overflow-hidden'>
-            <motion.span
-              variants={item}
-              // whileHover={'hover'}
-              className='inline-block'
-            >
+            <motion.span className='inline-block'>
               <span className='transition-all duration-500 ease-in-out hover:font-extrabold'>
                 Nascimento
               </span>
